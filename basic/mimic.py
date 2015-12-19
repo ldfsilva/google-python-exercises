9bbd3a3cf1dc97c5c5ccf1958602a73832bc4d43
Leandro Ferreira da Silva <ldfsilva@gmail.com>
2015-12-19 03:12:54 -0600

    mimic resolved

--- basic/mimic.py
+++ basic/mimic.py
@@ -44,17 +44,47 @@ columns, so the output looks better.
 import random
 import sys
 
-
 def mimic_dict(filename):
-  """Returns mimic dict mapping each word to list of words which follow it."""
-  # +++your code here+++
-  return
+    """Returns mimic dict mapping each word to list of words which follow it."""
+    m_dict = {}
+    with open(filename, 'r') as fp:
+        data = ' '.join(fp.read().split()).replace('!', '').replace('.', '') \
+            .replace(',', '').replace('-', '').replace('?', '').replace(';', '') \
+            .replace('`', '').replace(':', '').replace('_', '').replace('"', '') \
+            .replace("'", '').replace('(', '').replace(')', '')
+        data = data.split(' ')
+
+        # ? How to iterate and get index same time in for loop ? Going with while for now
+        while len(data) >= 2:
+            if data[0] in m_dict:
+                tmp_list = m_dict.get(data[0])
+                tmp_list.append(data[1])
+            else:
+                m_dict[data[0]] = [data[1]]
+
+            # Remove 1st element to always work in the beginning of the list
+            data.pop(0)
+
+    return m_dict
 
 
 def print_mimic(mimic_dict, word):
-  """Given mimic dict and start word, prints 200 random words."""
-  # +++your code here+++
-  return
+    """Given mimic dict and start word, prints 200 random words."""
+    line, text = '', ''
+
+    # Iterating 200 time to pick up random keys and values
+    for count in range(0, 200):
+        key = random.choice(list(mimic_dict.keys()))
+        val = mimic_dict.get(key)
+        line += ('{} {} '.format(key, random.choice(val)))
+
+        # print 70 columns per line
+        if len(line) > 70:
+            text += line + '\n'
+            line = ''
+
+    print(text)
+    return True
 
 
 # Provided main(), calls mimic_dict() and mimic()
